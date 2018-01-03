c6f4a01dd69b3a06b1c907ce535627c582f9f6df
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-01-02 22:17:27 -0200

    Documentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Docs from './docs/Docs';
 import './index.css';
-import App from './App';
+import '../node_modules/highlight.js/styles/ocean.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Docs />, document.getElementById('root'));
